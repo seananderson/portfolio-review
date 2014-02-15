9facc611a1f02a1522f3c9e60a52c85576c3b2ea
Sean Anderson <github@seananderson.ca>
2014-02-14 18:21:45 -0800

    Updates to risk fig; add risk fig to paper

--- risk-fig/make-risk-fig.R
+++ risk-fig/make-risk-fig.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 axis.col <- "grey30"
-line.col <- "grey50"
+line.col <- "grey65"
 # Some variance and risk metrics:
 
 # Variance at Risk:
@@ -50,7 +50,7 @@ plot_sim_skew <- function(n = 165, mean = 0, sd = 0.7, xi = 1,
   mtext(risk.level, side = 3, line = -1.20, adj = 0.02, cex = 0.8, col = axis.col)
 }
 
-pdf("risk-fig.pdf", width = 7.0, height = 4.5)
+pdf("risk-fig.pdf", width = 7.0, height = 4.3)
 # setup panel layout:
 split.screen(c(1, 2))
 split.screen(c(3, 1), screen = 1)
@@ -70,30 +70,37 @@ left_par <- function() {
   par(las = 1)
   par(tck = -0.017)
   par(mgp = c(2, 0.4, 0))
-  par(cex = 0.7, mar = c(0, 0, 0, 3), oma = c(6, 2.5, 2, 1))
+  par(cex = 0.7, mar = c(0, 0, 0, 3), oma = c(5.75, 3.0, 2, 1))
 }
 
 right_par <- function() {
   par(las = 1)
   par(tck = -0.017)
   par(mgp = c(2, 0.4, 0))
-  par(cex = 0.7, mar = c(0, 1.5, 0, 0), oma = c(6, 2.5, 2, 1))
+  par(cex = 0.7, mar = c(0, 1.5, 0, 0), oma = c(5.75, 3.0, 2, 1))
 }
 
 # Downside risk vs. symmetric variability simulation panel:
 set.seed(140)
 screen(3);left_par()
 plot_sim_skew(xi = 10000, x.axis = FALSE, risk.level = "(a) Low risk")
-mtext("Simulated time series - same CV, different risk", side = 3, line = 0.5, outer = FALSE, cex = 0.8, col = axis.col)
+mtext("Simulated time series; same CV, different risk", side = 3, line = 0.5, outer = FALSE, cex = 0.8, col = axis.col)
 screen(4);left_par()
 plot_sim_skew(xi = 1, x.axis = FALSE, risk.level = "(b) Medium risk")
 screen(5);left_par()
 plot_sim_skew(xi = 0.0001, x.axis = TRUE, risk.level = "(c) High risk")
 par(xpd = NA)
-mtext("Time", side = 1, line = 1.0, outer = FALSE, cex = 0.8, col = axis.col)
+mtext("Time", side = 1, line = 0.6, outer = FALSE, cex = 0.8, col = axis.col)
 par(las = 0)
-mtext("Abundance or growth rate", side = 2, line = 1.0, outer = T, cex = 0.8, col = axis.col)
+mtext("Abundance or growth rate", side = 2, line = 0.5, outer = T, cex = 0.8, col = axis.col)
+
 par(las = 1)
+text(2, 0.7, expression(sigma), pos = 2, cex = 1.1, col = axis.col)
+text(2, 0, expression(mu), pos = 2, cex = 1.1, col = axis.col)
+text(2, -1.1, "VaR", pos = 2, cex = 1.1, col = axis.col)
+text(2, -1.9, "CVaR", pos = 2, cex = 1.1, col = axis.col)
+
+
 par(xpd = FALSE)
 
 # Fraser River downside risk vs. symmetric variability panel
@@ -124,9 +131,8 @@ plot(1, 1, type = "n", xlim = c(c(1, nrow(fr_risk)) + c(-0.1, 0.1)), ylim
   = c(0, 1), yaxs = "i", xaxs = "i", xlab = "", ylab = "",
   xaxt = "n", axes = F)
 axis(2, col = line.col, col.axis = axis.col)
-box(col = "grey40", lwd = 1)
+box(col = line.col, lwd = 1)
 
-#col <- c("#000000", col)
 lty <- rep(1, 99)
 lwd <- rep(2.4, 99)
 
@@ -143,23 +149,21 @@ for(ii in risk_types) {
   column_i <- which(ii == names(fr_risk))
   lines(1:nrow(fr_risk), fr_risk[,column_i], col = colour,
     lwd = lwd[i], lty = lty[i])
-  #points(1:nrow(fr_risk), fr_risk[,column_i], col = colour, pch = 20,
-    #cex = 1.4)
 }
 par(las = 2)
 axis(1, at = 1:nrow(fr_risk), labels = rownames(fr_risk), col = line.col, mgp = c(2, 0.5, 0), cex.axis = 1.1, col.axis = axis.col)
 legend_order <- c(1, 2, 3, 4)
-legend(3, 1.02, risk_types_pretty[legend_order], col = colours_[legend_order], lwd = lwd[legend_order], lty = lty[legend_order], bty = "n",
-  cex = 1, text.col = axis.col)
+legend(3, 1.03, risk_types_pretty[legend_order], col = colours_[legend_order], lwd = lwd[legend_order], lty = lty[legend_order], bty = "n",
+  cex = 1.05, text.col = axis.col)
 mtext("Relative risk or variability", side = 2, cex = 0.8, las = 0, line = 2.1, col = axis.col)
 mtext("(e)", side = 3, adj = 0.01, col = axis.col, cex = 0.8, las = 0, line = -1.2)
 par(xpd = NA)
 
 mtext("<Insert map here>", side = 3, line = 6, las = 0, cex = 0.8, col = axis.col)
-mtext("(d)", side = 3, adj = 0.01, col = axis.col, cex = 0.8, las = 0, line = 11)
 
 screen(6);right_par()
 mtext("Fraser River sockeye salmon", side = 3, line = 0.5, outer = FALSE, cex = 0.8, col = axis.col)
+mtext("(d)", side = 3, adj = 0.075, col = axis.col, cex = 0.8, las = 0, line = -1.2)
 
 close.screen(all.screens = TRUE)
 
